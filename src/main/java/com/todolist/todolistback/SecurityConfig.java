8c7c0d716d04930fbb03bee54c038eb6a0901c7e
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2025-12-11 11:12:35 +0100

    feat(login): added login route

--- src/main/java/com/todolist/todolistback/SecurityConfig.java
+++ src/main/java/com/todolist/todolistback/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.todolist.todolistback;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +23,15 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             );
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/todolist/todolistback/controller/AuthentificationController.java
+++ src/main/java/com/todolist/todolistback/controller/AuthentificationController.java
@@ -3,12 +3,18 @@ package com.todolist.todolistback.controller;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.todolist.todolistback.security.JwtUtil;
 import com.todolist.todolistback.user.User;
 import com.todolist.todolistback.user.UserRepository;
 
@@ -16,10 +22,33 @@ import com.todolist.todolistback.user.UserRepository;
 @RequestMapping("/auth")
 public class AuthentificationController {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    private AuthenticationManager authenticationManager;
+    private final BCryptPasswordEncoder encoder;
+    private JwtUtil jwtUtils;
 
-    public AuthentificationController(UserRepository userRepository) {
+    public AuthentificationController(UserRepository userRepository, AuthenticationManager authenticationManager,
+            BCryptPasswordEncoder encoder, JwtUtil jwtUtils) {
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.encoder = encoder;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    user.getUsername(),
+                    user.getPassword()
+                )
+            );
+            
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(jwtUtils.generateToken(userDetails.getUsername()));
+        } catch (BadCredentialsException badCredentialsException) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Incorrect credentials");
+        }
     }
 
     @PostMapping("/register")
