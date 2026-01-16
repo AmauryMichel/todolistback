8aa1d1694e5618e1b422e9976a8e362fa23b5dbc
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-01-16 11:51:32 +0100

    fix(token): edited cors settings

--- src/main/java/com/todolist/todolistback/SecurityConfig.java
+++ src/main/java/com/todolist/todolistback/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.todolist.todolistback;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,7 +28,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-            .cors(AbstractHttpConfigurer::disable);
+            .cors(Customizer.withDefaults());
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/register", "/auth/login").permitAll()
                 .anyRequest().authenticated()
